e6d6a2d944c87b99316fc35c1f5efa2a833d3d49
DorianGC-G <dorian.guitel@gmail.com>
2022-01-07 17:58:31 +0100

    get data from the API using axios, then save it in dashboard component's state and session storage

--- app/javascript/components/Dashboard/Dashboard.js
+++ app/javascript/components/Dashboard/Dashboard.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 // Styles
 import { Layout, Wrapper } from './Dashboard.styles'
@@ -10,6 +11,27 @@ import Selector from "./Selector/Selector"
 import Summary from "./Summary/Summary"
 
 const Dashboard = () => {
+  const [memories, setMemories] = useState([])
+
+  // Get data from localStorage if present
+  const sessionState = sessionStorage.getItem('memoriesState')
+
+  useEffect(() => {
+    // Only call the API if data isn't in sessionStorage, else use sessionStorage
+    if (sessionState == "[]" || sessionState == null) {
+      axios.get('/api/v1/index')
+        .then( resp => {
+          setMemories(resp.data)
+          sessionStorage.setItem('memoriesState', JSON.stringify(memories))
+        })
+        .catch(
+          resp => console.log(resp)
+        )
+    } else {
+      setMemories(JSON.parse(sessionState))
+    }
+  }, [memories.length])
+
   return(
     <Layout>
       <Wrapper>
