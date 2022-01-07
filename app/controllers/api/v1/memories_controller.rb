b9fe8160f47e8f00665a9a80cf93abefb8fc3e26
DorianGC-G <dorian.guitel@gmail.com>
2022-01-07 16:06:49 +0100

    data is now accessible at /api/v1/index as json

--- app/controllers/api/v1/memories_controller.rb
+++ app/controllers/api/v1/memories_controller.rb
@@ -1,2 +1,35 @@
 class Api::V1::MemoriesController < ApplicationController
+  def index
+
+    # Pick every country for database
+    countries = Memory.distinct.pluck(:country)
+    
+    data = []
+
+    # Calculations on all data
+    total_data = {}
+    total_data[:country] = "All"
+    total_data[:revenue] = Memory.sum("quantity * unit_price").round(2)
+    total_data[:unique_orders] = Memory.distinct.count(:order_id)
+    total_data[:average_revenue_per_order] = (total_data[:revenue] / total_data[:unique_orders]).round(2)
+    total_data[:unique_customers] = Memory.distinct.count(:customer_id)
+    total_data[:revenue_per_month] = Memory.all.group("DATE_TRUNC('year', date)", "DATE_TRUNC('month', date)").sum("quantity * unit_price")
+    data << total_data
+
+    # Calculations on every country's data
+    countries.each do |country|
+      country_memories = Memory.where(country: country)
+      country_data = {}
+      country_data[:country] = country
+      country_data[:revenue] = country_memories.sum("quantity * unit_price").round(2)
+      country_data[:unique_orders] = country_memories.distinct.count(:order_id)
+      country_data[:average_revenue_per_order] = (country_data[:revenue] / country_data[:unique_orders]).round(2)
+      country_data[:unique_customers] = country_memories.distinct.count(:customer_id)
+      country_data[:revenue_per_month] = country_memories.group("DATE_TRUNC('year', date)", "DATE_TRUNC('month', date)").sum("quantity * unit_price")
+      data << country_data
+    end
+    
+    # Render data as JSON so we can exploit it with React
+    render json: data.to_json
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   root 'pages#dashboard'
   namespace :api, defaults: { format: 'json' } do
     namespace :v1 do
-      resources :memories
+      resources :memories, only: :index
+      get 'index', to: "memories#index"
     end
   end
 end
